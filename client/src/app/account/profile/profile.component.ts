ff86d9ceeda36e08e13e01c932d8031027f6ccd5
Kakelys <alexglobal1459@gmail.com>
2024-04-01 09:23:08 +0300

    add sending messign on profile page

--- client/src/app/account/profile/profile.component.ts
+++ client/src/app/account/profile/profile.component.ts
@@ -7,6 +7,14 @@ import { User } from 'src/shared/models/user.model';
 import { Roles } from 'src/shared/roles.enum';
 import { AdminService } from 'src/app/forum/admin/services/admin.service';
 import { environment } from 'src/environments/environment';
+import { ChatService } from 'src/app/chat/services/chat.service';
+import { HttpExceptionInterceptor } from 'src/shared/error/http-exception.interceptor';
+import { HttpException } from 'src/shared/models/http-exception.model';
+import { ToastrExtension } from 'src/shared/toastr.extension';
+import { ToastrService } from 'ngx-toastr';
+import { Chat } from 'src/app/chat/models/chat-model';
+import { NgForm } from '@angular/forms';
+import { NgFormExtension } from 'src/shared/ng-form.extension';
 
 @Component({
   selector: 'app-profile',
@@ -20,6 +28,7 @@ export class ProfileComponent implements OnDestroy {
   profile: any = null;
   userId = null;
   roles = Roles;
+  pChat: Chat = null;
 
   private destroy$ = new ReplaySubject<boolean>(1);
 
@@ -27,8 +36,10 @@ export class ProfileComponent implements OnDestroy {
     private accountService: AccountService,
     private route: ActivatedRoute,
     private adminService: AdminService,
+    private chatService: ChatService,
+    private toastr: ToastrService,
     router: Router,
-    authService: AuthService,
+    authService: AuthService
   ) {
 
     authService.user$
@@ -60,11 +71,48 @@ export class ProfileComponent implements OnDestroy {
     this.userId = newId;
 
     this.accountService.getAccount(this.userId)
-      .subscribe((user: any) => {
-        this.adminService.user = user;
-        this.profile = user;
+      .subscribe({
+        next: (user: any) => {
+          this.adminService.user = user;
+          this.profile = user;
+
+          if(this.user)
+            this.updateChatBetween();
+        },
+        error: (err: HttpException) => {
+          ToastrExtension.handleErrors(this.toastr, err.errors);
+        }
       });
   };
+
+  updateChatBetween() {
+    this.chatService.getChatBetween(this.profile.id)
+      .subscribe({
+        next: (chat: Chat | null) => {
+          this.pChat = chat;
+        },
+        error: (err: HttpException) => {
+          ToastrExtension.handleErrors(this.toastr, err.errors);
+        }
+      })
+  }
+
+  onStartChat(form: NgForm) {
+    if(form.invalid) {
+      NgFormExtension.markAllAsTouched(form);
+      return;
+    }
+
+    this.chatService.createPersonal(form.value.content, this.profile.id)
+    .subscribe({
+      next: (chat: Chat) => {
+        //navigate to chat?
+      },
+      error: (err: HttpException) => {
+        ToastrExtension.handleErrors(this.toastr, err.errors);
+      }
+    })
+  }
 
   ngOnDestroy(): void {
     this.destroy$.next(true);
