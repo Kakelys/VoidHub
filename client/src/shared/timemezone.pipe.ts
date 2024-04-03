69a1ddbb1f5ab199c5c8463e87c1de07ddde25de
Kakelys <alexglobal1459@gmail.com>
2024-04-03 09:45:15 +0300

    add date timezone pipe

---
+++ client/src/shared/timemezone.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  standalone: true,
+  name: 'withtimezone'
+})
+export class TimezonePipe implements PipeTransform {
+  transform(utcDate: Date) : Date {
+    utcDate = new Date(utcDate);
+    const timezoneOffsetHours = new Date().getTimezoneOffset() / 60;
+    const hoursToAddOrSubtract = -timezoneOffsetHours;
+
+    utcDate.setHours(utcDate.getHours() + hoursToAddOrSubtract)
+    return utcDate;
+  }
+}
