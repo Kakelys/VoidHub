420271606bfbb017a62e3bd73d4a692d849e2016
Vlad <83510308+Kakelys@users.noreply.github.com>
2024-09-11 21:51:49 +0300

    Added filter dtos

---
+++ API/ForumApi/DTO/Filter/FilterResult.cs
@@ -0,0 +1,7 @@
+namespace ForumApi.DTO.Filter;
+
+public class FilterResult<T>
+{
+    public int Total { get; set; }
+    public object Data { get; set; }
+}
--- API/ForumApi/DTO/Forum/ForumDto.cs
+++ API/ForumApi/DTO/Forum/ForumDto.cs
@@ -1,12 +1,11 @@
-namespace ForumApi.DTO.DForum
+namespace ForumApi.DTO.DForum;
+
+public class ForumDto
 {
-    public class ForumDto
-    {
-        public int Id { get; set; }
-        public int SectionId { get; set; }
-        public string Title { get; set; } = null!;
-        public DateTime? DeletedAt { get; set; }
-        public bool IsClosed { get; set; }
-        public string ImagePath { get; set; } = null!;
-    }
+    public int Id { get; set; }
+    public int SectionId { get; set; }
+    public string Title { get; set; } = null!;
+    public DateTime? DeletedAt { get; set; }
+    public bool IsClosed { get; set; }
+    public string ImagePath { get; set; } = null!;
 }
